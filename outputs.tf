13940d8ec8b93f2aef1bcca749d163e42891c7d1
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,40 +1,35 @@
-output "api_management_openid_connect_providers" {
-  description = "All api_management_openid_connect_provider resources"
-  value       = azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers
-  sensitive   = true
-}
 output "api_management_openid_connect_providers_api_management_name" {
-  description = "List of api_management_name values across all api_management_openid_connect_providers"
-  value       = [for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : v.api_management_name]
+  description = "Map of api_management_name values across all api_management_openid_connect_providers, keyed the same as var.api_management_openid_connect_providers"
+  value       = { for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : k => v.api_management_name }
 }
 output "api_management_openid_connect_providers_client_id" {
-  description = "List of client_id values across all api_management_openid_connect_providers"
-  value       = [for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : v.client_id]
+  description = "Map of client_id values across all api_management_openid_connect_providers, keyed the same as var.api_management_openid_connect_providers"
+  value       = { for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : k => v.client_id }
   sensitive   = true
 }
 output "api_management_openid_connect_providers_client_secret" {
-  description = "List of client_secret values across all api_management_openid_connect_providers"
-  value       = [for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : v.client_secret]
+  description = "Map of client_secret values across all api_management_openid_connect_providers, keyed the same as var.api_management_openid_connect_providers"
+  value       = { for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : k => v.client_secret }
   sensitive   = true
 }
 output "api_management_openid_connect_providers_description" {
-  description = "List of description values across all api_management_openid_connect_providers"
-  value       = [for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : v.description]
+  description = "Map of description values across all api_management_openid_connect_providers, keyed the same as var.api_management_openid_connect_providers"
+  value       = { for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : k => v.description }
 }
 output "api_management_openid_connect_providers_display_name" {
-  description = "List of display_name values across all api_management_openid_connect_providers"
-  value       = [for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : v.display_name]
+  description = "Map of display_name values across all api_management_openid_connect_providers, keyed the same as var.api_management_openid_connect_providers"
+  value       = { for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : k => v.display_name }
 }
 output "api_management_openid_connect_providers_metadata_endpoint" {
-  description = "List of metadata_endpoint values across all api_management_openid_connect_providers"
-  value       = [for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : v.metadata_endpoint]
+  description = "Map of metadata_endpoint values across all api_management_openid_connect_providers, keyed the same as var.api_management_openid_connect_providers"
+  value       = { for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : k => v.metadata_endpoint }
 }
 output "api_management_openid_connect_providers_name" {
-  description = "List of name values across all api_management_openid_connect_providers"
-  value       = [for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : v.name]
+  description = "Map of name values across all api_management_openid_connect_providers, keyed the same as var.api_management_openid_connect_providers"
+  value       = { for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : k => v.name }
 }
 output "api_management_openid_connect_providers_resource_group_name" {
-  description = "List of resource_group_name values across all api_management_openid_connect_providers"
-  value       = [for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_management_openid_connect_providers, keyed the same as var.api_management_openid_connect_providers"
+  value       = { for k, v in azurerm_api_management_openid_connect_provider.api_management_openid_connect_providers : k => v.resource_group_name }
 }
 
